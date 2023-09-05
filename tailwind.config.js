a42d5ffa3f3851d1750a0ec5d40418911ba93c52
Pedro Enciso <penciso@ucsd.edu>
2023-09-05 16:59:33 -0700

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "rgb(var(--black))",
+      veryDarkGray: "rgb(var(--very-dark-gray))",
+      darkGray: "rgb(var(--dark-gray))",
+      linesDark: "rgb(var(--lines-dark))",
+      mediumGray: "rgb(var(--medium-gray))",
+      linesLight: "rgb(var(--lines-light))",
+      lightGray: "rgb(var(--light-gray))",
+      white: "rgb(var(--white))",
+      mainPurple: "rgb(var(--main-purple))",
+      mainPurpleHover: "rgb(var(--main-purple-hover))",
+      red: "rgb(var(--red))",
+      redHover: "rgb(var(--red-hover))",
+    },
     extend: {
       fontFamily: {
         jakarta: ["Plus Jakarta Sans", "sans-serif"],
