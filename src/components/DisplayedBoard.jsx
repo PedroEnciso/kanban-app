2a67f31593d110677364eb83b2ae1ffdf64b32b0
Pedro Enciso <penciso@ucsd.edu>
2023-09-06 16:25:45 -0700

    empty board component & styles

--- src/components/DisplayedBoard.jsx
+++ src/components/DisplayedBoard.jsx
@@ -4,7 +4,6 @@ import EmptyBoardMessage from "./EmptyBoardMessage";
 
 function DisplayedBoard({ board }) {
   // check if the board has any columns to display. if they do, display each column as a component. otherwise, display the component showing no data available
-  return <EmptyBoardMessage />;
   if (board.columns.length === 0) return <EmptyBoardMessage />;
   return <div>hello</div>;
 }
--- src/components/EmptyBoardMessage.jsx
+++ src/components/EmptyBoardMessage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function EmptyBoardMessage(props) {
-  return <div>this is an empty board</div>;
+function EmptyBoardMessage() {
+  return (
+    <div className="flex-grow flex flex-col justify-center items-center mt-[-48px] px-4">
+      <p className="text-center text-mediumGray font-bold text-lg">
+        This board is empty. Create a new column to get started.
+      </p>
+      <button className="text-[15px] font-bold text-white py-4 px-5 mt-6 bg-mainPurple rounded-[24px]">
+        +Add New Column
+      </button>
+    </div>
+  );
 }
 
 export default EmptyBoardMessage;
