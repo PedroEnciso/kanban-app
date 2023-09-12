5d96863571cd9fa0bd810254ceed26be69a55f2e
Pedro Enciso <penciso@ucsd.edu>
2023-09-12 16:49:35 -0700

    inherit text color

--- src/components/UI/Typography/HeadingL.jsx
+++ src/components/UI/Typography/HeadingL.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function HeadingL({ children }) {
-  return (
-    <span className="font-bold text-black text-lg leading-[1.28]">
-      {children}
-    </span>
-  );
+  return <span className="font-bold text-lg leading-[1.28]">{children}</span>;
 }
 
 export default HeadingL;
