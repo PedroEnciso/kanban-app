8eaca08ba720dabed6b796da61b9bc0100921b62
Pedro Enciso <penciso@ucsd.edu>
2023-09-20 15:22:14 -0700

    +new column styles

--- src/components/Column/ColumnContainer.jsx
+++ src/components/Column/ColumnContainer.jsx
@@ -9,11 +9,23 @@ function ColumnContainer() {
 
   if (displayColumns.length === 0) return <EmptyBoardMessage />;
 
+  const newColumnHandler = () => {
+    console.log("adding a new column!");
+  };
+
   return (
     <div className="flex gap-6 px-4 overflow-x-scroll">
       {boards[displayBoardIndex].columns.map((column) => (
         <Column key={column.id} column={column} columnList={displayColumns} />
       ))}
+      <div className="flex items-center bg-darkGray w-[280px] mt-12 shrink-0 rounded-md ">
+        <button
+          onClick={newColumnHandler}
+          className="w-full py-8 text-mediumGray hover:text-mainPurple transition-colors duration-300"
+        >
+          +New Column
+        </button>
+      </div>
     </div>
   );
 }
