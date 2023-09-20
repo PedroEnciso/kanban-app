832ead218ea13afb26ac2c33e314d860b4501666
Pedro Enciso <penciso@ucsd.edu>
2023-09-20 15:26:43 -0700

    +new column light background

--- src/components/Column/ColumnContainer.jsx
+++ src/components/Column/ColumnContainer.jsx
@@ -18,7 +18,7 @@ function ColumnContainer() {
       {boards[displayBoardIndex].columns.map((column) => (
         <Column key={column.id} column={column} columnList={displayColumns} />
       ))}
-      <div className="flex items-center bg-darkGray w-[280px] mt-12 shrink-0 rounded-md ">
+      <div className="flex items-center bg-linesLight dark:bg-darkGray w-[280px] mt-12 shrink-0 rounded-md ">
         <button
           onClick={newColumnHandler}
           className="w-full py-8 text-mediumGray hover:text-mainPurple transition-colors duration-300"
