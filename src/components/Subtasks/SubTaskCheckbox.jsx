c2a6eeac417c55b52cc88c5f7c5440be9cc3c6b3
Pedro Enciso <penciso@ucsd.edu>
2023-09-25 15:23:01 -0700

    removed state, using react-hook-form

--- src/components/Subtasks/SubTaskCheckbox.jsx
+++ src/components/Subtasks/SubTaskCheckbox.jsx
@@ -1,32 +1,20 @@
-import { useState } from "react";
-
 import BodyM from "../UI/Typography/BodyM";
 
-function SubTaskCheckbox({ subtask }) {
-  const [isChecked, setIsChecked] = useState(subtask.isCompleted);
-
-  const handleOnChange = () => {
-    setIsChecked((prevState) => !prevState);
-  };
-
+function SubTaskCheckbox({ subtask, register }) {
   return (
     <div className="flex px-3 py-3 gap-3 rounded bg-lightGray dark:bg-veryDarkGray hover:bg-mainPurple/25 dark:hover:bg-mainPurple/25">
       <input
+        {...register(subtask.title)}
         type="checkbox"
         name={subtask.title}
-        checked={isChecked}
-        onChange={handleOnChange}
+        defaultChecked={subtask.isCompleted}
         className="appearance-none peer w-4 h-4 border border-mediumGray/25 rounded-sm bg-white dark:bg-darkGray focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-mainPurple checked:bg-mainPurple dark:checked:bg-mainPurple checked:border-0"
       />
-      <label htmlFor={subtask.title} onClick={handleOnChange} className="grow">
-        <BodyM
-          extraClasses={isChecked && "line-through"}
-          textColor={
-            !isChecked ? "text-black dark:text-white" : "text-mediumGray"
-          }
-        >
-          {subtask.title}
-        </BodyM>
+      <label
+        htmlFor={subtask.title}
+        className="grow text-black dark:text-white peer-checked:line-through peer-checked:text-mediumGray"
+      >
+        <BodyM textColor="">{subtask.title}</BodyM>
       </label>
       <svg
         className="absolute w-4 h-4 mt-1 ml-0.5 hidden peer-checked:block pointer-events-none"
