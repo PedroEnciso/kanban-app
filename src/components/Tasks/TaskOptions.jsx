60c62a1afc8a2fd42919e01a086d8025e8ed3dbe
Pedro Enciso <penciso@ucsd.edu>
2023-09-28 10:08:49 -0700

    created component

---
+++ src/components/Tasks/TaskOptions.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function TaskOptions({ onDeleteTask }) {
+  const editTaskHandler = () => {
+    console.log("show edit screen");
+  };
+
+  const deleteTaskHandler = () => {
+    onDeleteTask();
+  };
+
+  return (
+    <div className="absolute top-full left-full translate-x-[-100%] z-50 w-[182px] flex flex-col bg-white rounded-lg text-[13px]">
+      <button
+        className="w-full text-left py-4 px-4 text-mediumGray rounded-tl-lg rounded-tr-lg hover:bg-lightGray transition-colors duration-200 "
+        onClick={editTaskHandler}
+      >
+        Edit Task
+      </button>
+      <button
+        className="w-full text-left py-4 px-4 text-red rounded-bl-lg rounded-br-lg hover:bg-red/10 transition-colors duration-200"
+        onClick={deleteTaskHandler}
+      >
+        DeleteTask
+      </button>
+    </div>
+  );
+}
+
+export default TaskOptions;
