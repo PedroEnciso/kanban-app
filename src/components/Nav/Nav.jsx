65ecb6dd84735eaae1cd017b5b94ad95da424b8e
Pedro Enciso <penciso@ucsd.edu>
2023-10-16 16:23:32 -0700

    show edit/delete board options

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,13 +9,15 @@ import logo from "../../assets/logo-mobile.svg";
 import chevronDown from "../../assets/icon-chevron-down.svg";
 import plus from "../../assets/icon-add-task-mobile.svg";
 import ellipses from "../../assets/icon-vertical-ellipsis.svg";
+import BoardFocusContainer from "../Boards/BoardFocusContainer";
 
 function Nav() {
   const [isShowingModal, setIsShowingModal] = useState(false);
   const [isShowingBoardController, setIsShowingBoardController] =
     useState(false);
-  const boardCtx = useContext(BoardContext);
-  const { boards, displayBoardIndex } = boardCtx;
+  const [isShowingBoardOptions, setIsShowingBoardOptions] = useState(false);
+  const { boards, displayBoardIndex, displayColumns } =
+    useContext(BoardContext);
 
   const handleToggleBoardController = () => {
     setIsShowingBoardController((prev) => !prev);
@@ -25,9 +27,13 @@ function Nav() {
     setIsShowingModal((prev) => !prev);
   };
 
+  const handleToggleBoardOptions = () => {
+    setIsShowingBoardOptions((prev) => !prev);
+  };
+
   return (
     <>
-      <nav className="px-4 py-5 flex justify-between items-center bg-white dark:bg-darkGray">
+      <nav className="px-4 py-6 flex justify-between items-center bg-white dark:bg-darkGray">
         <div className="flex gap-4">
           <div>
             <img src={logo} alt="KanBan logo." />
@@ -44,7 +50,7 @@ function Nav() {
             )}
           </button>
         </div>
-        <div className="flex gap-4 items-center">
+        <div className="relative flex gap-4 items-center">
           <button
             className="bg-mainPurple hover:bg-mainPurpleHover disabled:bg-mainPurple/25 px-4 py-2.5 rounded-2xl"
             disabled={boards.length === 0}
@@ -52,9 +58,20 @@ function Nav() {
           >
             <img src={plus} alt="Add a task." />
           </button>
-          <button className="w-[4px] h-[16px] sm:w-auto sm:h-auto">
+          <button
+            onClick={handleToggleBoardOptions}
+            className="w-[4px] h-[16px] sm:w-auto sm:h-auto"
+          >
             <img src={ellipses} alt="" className="w-full" />
           </button>
+          {isShowingBoardOptions && (
+            <BoardFocusContainer
+              type="OPTIONS"
+              onClose={handleToggleBoardOptions}
+              currentBoard={boards[displayBoardIndex]}
+              columns={displayColumns}
+            />
+          )}
         </div>
       </nav>
       {isShowingModal && (
