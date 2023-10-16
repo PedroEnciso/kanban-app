dea49bd32ed0dcc268da7ebdadd557d86299c1e4
Pedro Enciso <penciso@ucsd.edu>
2023-10-16 16:24:02 -0700

    allow functionality of editing/deleting boards

--- src/components/Tasks/TaskOptions.jsx
+++ src/components/Tasks/TaskOptions.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 
-function TaskOptions({ onDeleteTask, onEditTask }) {
-  const editTaskHandler = () => {
-    onEditTask();
+function TaskOptions({ onDelete, onEdit, type = "TASK" }) {
+  let name = "Task";
+  if (type === "BOARD") name = "Board";
+  const editHandler = () => {
+    onEdit();
   };
 
-  const deleteTaskHandler = () => {
-    onDeleteTask();
+  const deleteHandler = () => {
+    onDelete();
   };
 
   return (
-    <div className="absolute top-full left-full translate-x-[-100%] z-50 w-[182px] flex flex-col bg-white dark:bg-veryDarkGray rounded-lg text-[13px]">
+    <div className="absolute top-full left-full translate-x-[-100%] z-50 w-[192px] flex flex-col bg-white dark:bg-veryDarkGray rounded-lg text-[13px]">
       <button
         className="w-full text-left py-4 px-4 text-mediumGray rounded-tl-lg rounded-tr-lg hover:bg-lightGray dark:hover:bg-darkGray transition-colors duration-200 "
-        onClick={editTaskHandler}
+        onClick={editHandler}
       >
-        Edit Task
+        Edit {name}
       </button>
       <button
         className="w-full text-left py-4 px-4 text-red rounded-bl-lg rounded-br-lg hover:bg-red/10 transition-colors duration-200"
-        onClick={deleteTaskHandler}
+        onClick={deleteHandler}
       >
-        DeleteTask
+        Delete {name}
       </button>
     </div>
   );
