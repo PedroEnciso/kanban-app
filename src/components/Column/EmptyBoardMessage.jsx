1265fad88cbe789fec8df2bc8cc7686cde583281
Pedro Enciso <penciso@ucsd.edu>
2023-10-16 17:18:19 -0700

    show edit board on button click

--- src/components/Column/EmptyBoardMessage.jsx
+++ src/components/Column/EmptyBoardMessage.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { useState, useContext } from "react";
 
-function EmptyBoardMessage() {
+import Modal from "../UI/Modal";
+import AddBoard from "../Boards/AddBoard";
+
+function EmptyBoardMessage({ board }) {
+  const [showEditBoard, setShowEditBoard] = useState(false);
+
+  const toggleHandler = () => {
+    setShowEditBoard((prev) => !prev);
+  };
   return (
     <div className="flex-grow flex flex-col justify-center items-center mt-[-48px] px-4">
       <p className="text-center text-mediumGray font-bold text-lg">
         This board is empty. Create a new column to get started.
       </p>
-      <button className="text-[15px] font-bold text-white py-4 px-5 mt-6 bg-mainPurple rounded-[24px]">
+      <button
+        onClick={toggleHandler}
+        className="text-[15px] font-bold text-white py-4 px-5 mt-6 bg-mainPurple rounded-[24px]"
+      >
         +Add New Column
       </button>
+      {showEditBoard && (
+        <Modal onModalClose={toggleHandler}>
+          <AddBoard onClose={toggleHandler} board={board} />
+        </Modal>
+      )}
     </div>
   );
 }
