e226fb37c01a80ea09f2f60a1e92546ce3b6920e
Pedro Enciso <penciso@ucsd.edu>
2023-10-19 12:00:19 -0700

    include Sidebar and OpenSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import Nav from "./components/Nav/Nav";
 import Layout from "./components/Layout/Layout";
 import ColumnContainer from "./components/Column/ColumnContainer";
 import BoardContextProvider from "./store/BoardContextProvider";
+import Sidebar from "./components/Layout/Sidebar";
+import OpenSidebar from "./components/Layout/OpenSidebar";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const toggleSidebarHandler = () => {
+    setShowSidebar((prev) => !prev);
+  };
+
+  const sidebarContent = showSidebar ? (
+    <Sidebar toggleSidebar={toggleSidebarHandler} />
+  ) : (
+    <OpenSidebar toggleSidebar={toggleSidebarHandler} />
+  );
+
   return (
     <BoardContextProvider>
-      <Layout>
+      <Layout showSidebar={showSidebar}>
+        {sidebarContent}
         <Nav />
         <ColumnContainer />
       </Layout>
