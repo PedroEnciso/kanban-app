f47d742a1d143651e7899ca370e7f7b66783591f
Pedro Enciso <penciso@ucsd.edu>
2023-10-19 12:03:09 -0700

    optional classes for showing sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,10 @@
-function Layout({ children }) {
-  return <div className="min-h-screen flex flex-col gap-6">{children}</div>;
+function Layout({ children, showSidebar }) {
+  const classes = showSidebar ? "md:show-sidebar" : "";
+  return (
+    <div className={`min-h-screen flex flex-col gap-6 md:gap-0 ${classes}`}>
+      {children}
+    </div>
+  );
 }
 
 export default Layout;
