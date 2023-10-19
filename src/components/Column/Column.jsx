23ae4efe23d8318d27e4583dc93cb239ee67f414
Pedro Enciso <penciso@ucsd.edu>
2023-10-19 13:12:16 -0700

    display column colors

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -2,13 +2,33 @@ import HeadingS from "../UI/Typography/HeadingS";
 import Task from "../Tasks/Task";
 
 function Column({ column, columnList }) {
+  let circleColor = "";
+  if (column.color === "lightblue") {
+    circleColor = "bg-lightblue";
+  }
+  if (column.color === "orange") {
+    circleColor = "bg-orange";
+  }
+  if (column.color === "lightred") {
+    circleColor = "bg-lightred";
+  }
+  if (column.color === "pink") {
+    circleColor = "bg-pink";
+  }
+  if (column.color === "green") {
+    circleColor = "bg-green";
+  }
+
   return (
     <div className="min-w-[280px]">
-      <h2>
-        <HeadingS>
-          {column.name} ({column.tasks.length})
-        </HeadingS>
-      </h2>
+      <div className="flex gap-3 items-center">
+        <div className={`${circleColor} h-4 w-4 rounded-full`}></div>
+        <h2>
+          <HeadingS>
+            {column.name} ({column.tasks.length})
+          </HeadingS>
+        </h2>
+      </div>
       <ul className="mt-6 flex flex-col gap-6">
         {column.tasks.map((task) => (
           <Task key={task.id} task={task} columnList={columnList} />
